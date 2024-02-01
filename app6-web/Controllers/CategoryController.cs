5cc328867610b1bc0d852691eff7f2fca03b24ef
amdad <am.dadvand@gmail.com>
2024-02-01 07:32:38 +0330

    Add project files.

---
+++ app6-web/Controllers/CategoryController.cs
@@ -0,0 +1,115 @@
+﻿using app6_web.Data;
+using app6_web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+
+namespace app6_web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //index
+        public IActionResult Index()
+        {
+            IEnumerable<Category> CtaegoryList = _db.Categories;
+            return View(CtaegoryList);
+        }
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //post
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder)
+            {
+                ModelState.AddModelError("Name", "مقدار هر دو قیلد نباید یکی باشد");
+
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "دسته با موفقیت ایجاد شد";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+
+        }
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categorySingel = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //post
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder)
+            {
+                ModelState.AddModelError("Name", "مقدار هر دو قیلد نباید یکی باشد");
+
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "دسته با موفقیت  ویرایش شد";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categorySingel = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //post
+        [HttpPost]
+        public IActionResult DeletePost(int? id)
+        {
+
+            var obj = _db.Categories.Find(id);
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "دسته با موفقیت  حذف شد";
+            return RedirectToAction("Index");
+
+
+        }
+
+
+
+    }
+}
---
+++ app6-web/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using app6_web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace app6_web.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+        public DbSet <Category> Categories { get; set; }
+    }
+}
